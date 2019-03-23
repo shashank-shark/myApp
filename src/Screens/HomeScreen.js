591d8cc030667fcdf77d3ca5c6aec17969546d8b
shashank-shark <shashankreddy231@gmail.com>
2019-03-23 09:36:43 +0530

    Update HomeScreen.js
    Fixed exporting problem.

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View,Button} from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
 import React from 'react'
 
-class HomeScreen extends React.Component {
+export default class HomeScreen extends React.Component {
     render() {
       return (
         <View style={styles.container}>  
