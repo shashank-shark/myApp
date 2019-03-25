3f36e10718b038f66fdc131e3cf3003df6332f9d
shashank-shark <shashankreddy231@gmail.com>
2019-03-25 13:44:32 +0530

    Create TopNavigation.js
    Top Material Navigation added to GalleryScreen.

---
+++ src/TopNavigators/TopNavigation.js
@@ -0,0 +1,46 @@
+import { createBottomTabNavigator, createAppContainer, createMaterialTopTabNavigator} from 'react-navigation'  
+import {StyleSheet, Text, View,Button} from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
+import React from 'react'
+
+// importing explicit screens
+import Feed from '../UDScreens/Feed'
+import Upload from '../UDScreens/Upload'
+
+
+const TabNavigator = createMaterialTopTabNavigator(  
+  {  
+      Feed: { screen: Feed,  
+          navigationOptions:{  
+              tabBarLabel:'Feed',  
+              tabBarIcon: ({ tintColor }) => (  
+                  <View>  
+                      <Icon style={[{color: tintColor}]} size={25} name={'ios-home'}/>  
+                  </View>),  
+          }  
+      },  
+      Upload: { screen: Upload,  
+          navigationOptions:{  
+              tabBarLabel:'Upload',  
+              tabBarIcon: ({ tintColor }) => (  
+                  <View>  
+                      <Icon style={[{color: tintColor}]} size={25} name={'ios-calendar'}/>  
+                  </View>),  
+              activeColor: '#f60c0d',  
+              inactiveColor: '#f65a22',  
+              barStyle: { backgroundColor: '#f69b31' },  
+          }  
+      },  
+  },  
+  {  
+    shifting: false,
+    tabBarPosition: 'top',
+    initialRouteName: "Feed",  
+    activeColor: '#f0edf6',  
+    inactiveColor: '#226557',  
+    barStyle: { backgroundColor: '#3BAD87' },
+    
+  },  
+);  
+
+export default createAppContainer(TabNavigator);  
