b1117f51d4bf96790b295b5e4e7130c74ac7159f
shashank-shark <shashankreddy231@gmail.com>
2019-03-25 13:01:10 +0530

    Update Feed.js
    Added new Component 1

--- src/UDScreens/Feed.js
+++ src/UDScreens/Feed.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react'
 import { FlatList, StyleSheet, Text, View, Image } from 'react-native'
 
 class Feed extends Component {
-    render () {
-        return(
-            <View></View>
+
+    constructor(props) 
+    {
+        super (props);
+    }
+
+
+    render () 
+    {
+        return
+        (
+            <View>
+                <Text>Hello World</Text>
+            </View>
         );
     }
 }
