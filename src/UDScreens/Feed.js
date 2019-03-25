1bad86f6a2b530c4b41d1c63a5c818d84452432f
shashank-shark <shashankreddy231@gmail.com>
2019-03-25 21:38:06 +0530

    Update Feed.js
    Added photo feed.

--- src/UDScreens/Feed.js
+++ src/UDScreens/Feed.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react'
 import { FlatList, StyleSheet, Text, View, Image } from 'react-native'
+import { white } from 'ansi-colors';
 
-export default class Upload extends Component {
-    render () {
+export default class Feed extends Component {
+
+    constructor(props)
+    {
+        super(props);
+    }
+
+    render () 
+    {
         return (
-            <View>
-                <Text>Upload Here</Text>
+            <View style={{flex: 1}}>
+                <View>
+                    <View>
+                        <Text>Time Ago</Text>
+                        <Text>@Teacher1</Text>
+                    </View>
+
+                    <View>
+                        <Image
+                        source={{uri: 'https://source.unsplash.com/random/500x' + Math.floor(((Math.random() * 800) + 500))}}
+                        style={styles.imageFeed}
+                        />
+                    </View>
+
+                    <View>
+                        <Text>Caption Text herer....</Text>
+                        <Text>View Comments .. </Text>
+                    </View>
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({  
+    imageFeed: {  
+        resizeMode: 'cover',
+        width: '100%',
+        height: 275,
+    },  
+  });
