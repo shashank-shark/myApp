169bc032d1e54cbf7a0d93233fa7a39a3cc66caf
shashank-shark <shashankreddy231@gmail.com>
2019-03-25 13:55:13 +0530

    Update GalleryScreen.js
    Fixed AndroidPageView warning.

--- src/Screens/GalleryScreen.js
+++ src/Screens/GalleryScreen.js
@@ -5,6 +5,17 @@ import Icon from 'react-native-vector-icons/Ionicons'
 import React from 'react'
 import TopNavigation from '../TopNavigators/TopNavigation'
 
+import { YellowBox } from 'react-native';
+import _ from 'lodash';
+
+YellowBox.ignoreWarnings(['ViewPagerAndroid']);
+const _console = _.clone(console);
+console.warn = message => {
+  if (message.indexOf('ViewPagerAndroid') <= -1) {
+    _console.warn(message);
+  }
+};
+
 export default class ProfileScreen extends React.Component {  
     render() {  
         return (  
