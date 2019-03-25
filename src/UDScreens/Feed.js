c66c659f280b00f01bf5f2e7c00472ea06530649
shashank-shark <shashankreddy231@gmail.com>
2019-03-25 11:50:56 +0530

    Update Feed.js
    Added Video and Photo Feed.

--- src/UDScreens/Feed.js
+++ src/UDScreens/Feed.js
@@ -1,6 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { FlatList, StyleSheet, Text, View, Image } from 'react-native'
 
 class Feed extends Component {
-    
+    render () {
+        return(
+            <View></View>
+        );
+    }
 }
+
+export default Feed;
