384cfe4125a80f6f53b8187d7fcfb7050afe7946
shashank-shark <shashankreddy231@gmail.com>
2019-03-25 13:01:31 +0530

    Update Upload.js
    Added new component 1.

---
+++ src/UDScreens/Upload.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { FlatList, StyleSheet, Text, View, Image } from 'react-native'
+
+class Feed extends Component {
+
+    constructor(props) 
+    {
+        super (props);
+    }
+
+
+    render () 
+    {
+        return
+        (
+            <View>
+                <Text>Hello World</Text>
+            </View>
+        );
+    }
+}
+
+export default Feed;
