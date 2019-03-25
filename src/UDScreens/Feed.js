acc3e38fa4cecd229193b87ea68c8ef91978e00a
shashank-shark <shashankreddy231@gmail.com>
2019-03-25 21:56:55 +0530

    Update Feed.js
    Added photo feed code to fetch dummy pics.

--- src/UDScreens/Feed.js
+++ src/UDScreens/Feed.js
@@ -7,13 +7,38 @@ export default class Feed extends Component {
     constructor(props)
     {
         super(props);
+
+        // adding states
+        this.state = {
+            photo_feed: [0,1,2,3,4],
+            refresh: false,
+        }
+    }
+
+    // Refresh or add new content
+    loadNew = () => {
+        this.setState ({
+            refresh: true,
+        });
+
+        this.setState ({
+            photo_feed: [5,6,7,8,9],
+            refresh: false
+        });
     }
 
     render () 
     {
         return (
             <View style={{flex: 1}}>
-                <View>
+            <FlatList
+            refreshing={this.state.refresh}
+            onRefresh={this.loadNew}
+            data={this.state.photo_feed}
+            keyExtractor={(item, index) => index.toString()}
+            style={styles.flatListHere}
+            renderItem={({item, index}) => (
+            <View key={index}>
                     <View>
                         <Text>Time Ago</Text>
                         <Text>@Teacher1</Text>
@@ -30,7 +55,11 @@ export default class Feed extends Component {
                         <Text>Caption Text herer....</Text>
                         <Text>View Comments .. </Text>
                     </View>
-                </View>
+            </View>
+
+            )}
+            />
+              
             </View>
         );
     }
@@ -41,5 +70,9 @@ const styles = StyleSheet.create({
         resizeMode: 'cover',
         width: '100%',
         height: 275,
-    },  
+    },
+    flatListHere: {
+        flex: 1,
+        backgroundColor: '#eee',
+    },
   });
