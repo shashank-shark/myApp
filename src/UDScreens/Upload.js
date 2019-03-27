996b1bf75355eba518b3fad9f70e7c20d8912a16
shashank-shark <shashankreddy231@gmail.com>
2019-03-27 21:58:15 +0530

    Update Upload.js
    Adding code to upload photos.

--- src/UDScreens/Upload.js
+++ src/UDScreens/Upload.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react'
 import { FlatList, StyleSheet, Text, View, Image } from 'react-native'
+import * as firebase from 'firebase';
+import 'firebase/firestore';
 
 export default class Upload extends Component {
 
     constructor(props)
     {
         super(props);
+        this.state = {
+            loggedin: false,
+
+        }
+    }
+
+    componentDidMount = () => {
+        var that = this
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                // Logged in
+                that.setState ({
+                    loggedin: true,
+                })
+            } else {
+                // Logged Out
+                that.setState ({
+                    loggedin: false,
+                })
+            }
+        })
     }
 
     render () 
     {
         return (
             <View>
-                <Text>Upload Here</Text>
+                { this.state.loggedin == true ? (
+                    // you are logged in
+                    <Text>Upload Page</Text>
+                ) : (
+                    // you are not logged in
+                    <View>
+                        <Text>You are not logged in</Text>
+                        <Text>Please Login to view your profile</Text>
+                    </View>
+                )}
             </View>
         );
     }
