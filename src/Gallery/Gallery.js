4756e6f5fe42a1a2ec41f1a6a70f1ac10f918a05
shashank-shark <shashankreddy231@gmail.com>
2019-04-02 23:28:16 +0530

    Added Dummy Gallery.
    Added Dummy Gallery.

---
+++ src/Gallery/Gallery.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Dimensions, View } from 'react-native';
+import PropTypes from 'prop-types';
+import ImageViewer from 'ImageViewer';
+
+import GalleryImage from './GalleryImage';
+
+export default class Gallery extends Component {
+  constructor(props) {
+    super(props);
+
+    this.openLightbox = (index) => {
+      this.setState({
+        index,
+        shown: true,
+      });
+    };
+
+    this.hideLightbox = () => {
+      this.setState({
+        index: 0,
+        shown: false,
+      });
+    };
+  }
+
+  state = {
+    index: 0,
+    shown: false,
+  };
+
+  render() {
+    const { images } = this.props;
+    const { index, shown } = this.state;
+
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+        }}
+      >
+
+        {(() =>
+          images.map((image, idx) =>
+            <GalleryImage
+              index={idx}
+              key={idx}
+              onPress={this.showLightbox}
+              uri={image.uri}
+            />
+        )())}
+
+        <ImageViewer          
+          shown={shown}          
+          imageUrls={images}          
+          onClose={this.hideLightbox}
+          index={index}        
+        />
+      </View>
+    );
+  }
+}
+
+Gallery.propTypes = {
+  images: PropTypes.array,
+};
--- src/UDScreens/VideoFeed.js
+++ src/UDScreens/VideoFeed.js
@@ -15,7 +15,7 @@ export default class VideoFeed extends Component {
     {
         return (
             <View>
-                <Text>VideoFeed Here</Text>
+                <Text>Gallery Here</Text>
             </View>
         );
     }
